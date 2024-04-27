0aa403f190c44690f1e370f54bf3398373e28c14
yuya-hanzawa <ckz1120@gmail.com>
2024-04-27 17:08:55 +0900

    convert_jstの説明を追加

--- convert_jst/main.sql
+++ convert_jst/main.sql
@@ -1,4 +1,7 @@
-CREATE TEMP FUNCTION convert_jst(input ANY TYPE)
+-- convert_jst: Convert UTC timestamp to JST timestamp.
+-- Input: UTC timestamp
+-- Output: JST timestamp in string format
+CREATE TEMP FUNCTION convert_jst(input TIMESTAMP)
 AS ((
   SELECT FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP(input), 'Asia/Tokyo')
 ));
