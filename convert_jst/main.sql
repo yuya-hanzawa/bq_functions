e603d6e5350703ce9c28ca2bec68cbc28d8c5027
yuya-hanzawa <ckz1120@gmail.com>
2024-04-27 16:14:29 +0900

    インデントを修正

--- convert_jst/main.sql
+++ convert_jst/main.sql
@@ -1,5 +1,4 @@
 CREATE TEMP FUNCTION convert_jst(input ANY TYPE)
 AS ((
-  SELECT
-    FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP(input), 'Asia/Tokyo')
+  SELECT FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP(input), 'Asia/Tokyo')
 ));
